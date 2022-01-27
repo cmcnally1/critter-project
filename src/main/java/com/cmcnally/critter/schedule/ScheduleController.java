cad91fcde62ada5607efc7865db31b5fe33d3c63
Ciaran McNally <ciaran.mcnally94@gmail.com>
2022-01-27 21:16:43 +0000

    Implemented schedule service methods

--- src/main/java/com/cmcnally/critter/schedule/ScheduleController.java
+++ src/main/java/com/cmcnally/critter/schedule/ScheduleController.java
@@ -1,7 +1,12 @@
 package com.cmcnally.critter.schedule;
 
+import com.cmcnally.critter.entity.Schedule;
+import com.cmcnally.critter.service.ScheduleService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -11,28 +16,85 @@ import java.util.List;
 @RequestMapping("/schedule")
 public class ScheduleController {
 
+    // Inject the schedule service
+    @Autowired
+    private ScheduleService scheduleService;
+
     @PostMapping
     public ScheduleDTO createSchedule(@RequestBody ScheduleDTO scheduleDTO) {
-        throw new UnsupportedOperationException();
+        // Create a schedule entity to be persisted from the DTO
+        Schedule scheduleToSave = convertScheduleDTOToEntity(scheduleDTO);
+        // Save the schedule via the service and convert the returned to DTO
+        return convertScheduleEntityToDTO(scheduleService.save(scheduleToSave));
     }
 
     @GetMapping
     public List<ScheduleDTO> getAllSchedules() {
-        throw new UnsupportedOperationException();
+        // Get all schedules via the service
+        List<Schedule> retrievedSchedules = scheduleService.findAllSchedules();
+        // Create a new list of DTOs to store values
+        List<ScheduleDTO> scheduleDTOs = new ArrayList<>();
+        // Loop through retrieved schedules, convert each to DTO and add to list
+        for (int i = 0; i < retrievedSchedules.size(); i++) {
+            scheduleDTOs.add(convertScheduleEntityToDTO(retrievedSchedules.get(i)));
+        }
+        // Return list
+        return scheduleDTOs;
     }
 
     @GetMapping("/pet/{petId}")
     public List<ScheduleDTO> getScheduleForPet(@PathVariable long petId) {
-        throw new UnsupportedOperationException();
+        // Get schedules that match id
+        List<Schedule> retrievedSchedules = scheduleService.findSchedulesForPet(petId);
+        // Create a new list of DTOs to store values
+        List<ScheduleDTO> scheduleDTOs = new ArrayList<>();
+        // Loop through retrieved schedules, convert each to DTO and add to list
+        for (int i = 0; i < retrievedSchedules.size(); i++) {
+            scheduleDTOs.add(convertScheduleEntityToDTO(retrievedSchedules.get(i)));
+        }
+        // Return list
+        return scheduleDTOs;
     }
 
     @GetMapping("/employee/{employeeId}")
     public List<ScheduleDTO> getScheduleForEmployee(@PathVariable long employeeId) {
-        throw new UnsupportedOperationException();
+        // Get schedules that match id
+        List<Schedule> retrievedSchedules = scheduleService.findSchedulesForEmployee(employeeId);
+        // Create a new list of DTOs to store values
+        List<ScheduleDTO> scheduleDTOs = new ArrayList<>();
+        // Loop through retrieved schedules, convert each to DTO and add to list
+        for (int i = 0; i < retrievedSchedules.size(); i++) {
+            scheduleDTOs.add(convertScheduleEntityToDTO(retrievedSchedules.get(i)));
+        }
+        // Return list
+        return scheduleDTOs;
     }
 
     @GetMapping("/customer/{customerId}")
     public List<ScheduleDTO> getScheduleForCustomer(@PathVariable long customerId) {
-        throw new UnsupportedOperationException();
+        // Get schedules that match id
+        List<Schedule> retrievedSchedules = scheduleService.findSchedulesForCustomer(customerId);
+        // Create a new list of DTOs to store values
+        List<ScheduleDTO> scheduleDTOs = new ArrayList<>();
+        // Loop through retrieved schedules, convert each to DTO and add to list
+        for (int i = 0; i < retrievedSchedules.size(); i++) {
+            scheduleDTOs.add(convertScheduleEntityToDTO(retrievedSchedules.get(i)));
+        }
+        // Return list
+        return scheduleDTOs;
+    }
+
+    // Method to convert schedule DTO to entity
+    private Schedule convertScheduleDTOToEntity(ScheduleDTO scheduleDTO) {
+        Schedule schedule = new Schedule();
+        BeanUtils.copyProperties(scheduleDTO, schedule);
+        return schedule;
+    }
+
+    // Method to convert schedule entity to DTO
+    private ScheduleDTO convertScheduleEntityToDTO(Schedule schedule) {
+        ScheduleDTO scheduleDTO = new ScheduleDTO();
+        BeanUtils.copyProperties(schedule, scheduleDTO);
+        return scheduleDTO;
     }
 }
