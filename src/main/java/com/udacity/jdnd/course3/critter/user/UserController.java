263bac9a62542561d3d6d68e6a2c82fbcd309723
Ciaran McNally <ciaran.mcnally94@gmail.com>
2022-01-27 18:26:35 +0000

    Implemented employee controller methods

--- src/main/java/com/udacity/jdnd/course3/critter/user/UserController.java
+++ src/main/java/com/udacity/jdnd/course3/critter/user/UserController.java
@@ -1,7 +1,9 @@
 package com.udacity.jdnd.course3.critter.user;
 
 import com.udacity.jdnd.course3.critter.entity.Customer;
+import com.udacity.jdnd.course3.critter.entity.Employee;
 import com.udacity.jdnd.course3.critter.service.CustomerService;
+import com.udacity.jdnd.course3.critter.service.EmployeeService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -23,6 +25,9 @@ public class UserController {
     // Inject customer service
     @Autowired
     private CustomerService customerService;
+    // Inject Employee service
+    @Autowired
+    private EmployeeService employeeService;
 
     @PostMapping("/customer")
     public CustomerDTO saveCustomer(@RequestBody CustomerDTO customerDTO){
@@ -56,22 +61,38 @@ public class UserController {
 
     @PostMapping("/employee")
     public EmployeeDTO saveEmployee(@RequestBody EmployeeDTO employeeDTO) {
-        throw new UnsupportedOperationException();
+        // Create an employee to save by converting the DTO to entity
+        Employee employeeToSave = convertEmployeeDTOToEntity(employeeDTO);
+        // Save the entity via the service
+        return convertEmployeeEntityToDTO(employeeService.save(employeeToSave));
     }
 
     @PostMapping("/employee/{employeeId}")
     public EmployeeDTO getEmployee(@PathVariable long employeeId) {
-        throw new UnsupportedOperationException();
+        // Retrieve employee by id
+        Employee retrievedEmployee = employeeService.getEmployeeById(employeeId);
+        // Return the employee as DTO
+        return convertEmployeeEntityToDTO(retrievedEmployee);
     }
 
     @PutMapping("/employee/{employeeId}")
     public void setAvailability(@RequestBody Set<DayOfWeek> daysAvailable, @PathVariable long employeeId) {
-        throw new UnsupportedOperationException();
+        // Set the employees availability via service
+        employeeService.setEmployeeAvailability(daysAvailable, employeeId);
     }
 
     @GetMapping("/employee/availability")
     public List<EmployeeDTO> findEmployeesForService(@RequestBody EmployeeRequestDTO employeeDTO) {
-        throw new UnsupportedOperationException();
+        // Get a list of employees by passing the required information from the request DTO
+        List<Employee> suitableEmployees = employeeService.findSuitableEmployees(employeeDTO.getSkills(), employeeDTO.getDate());
+        // Create a list of employee DTOs to be returned
+        List<EmployeeDTO> employeeDTOs = new ArrayList<>();
+        // Loop through the suitable employees and convert to DTOs
+        for (int i = 0; i < suitableEmployees.size(); i++) {
+            employeeDTOs.add(convertEmployeeEntityToDTO(suitableEmployees.get(i)));
+        }
+        // Return employees DTO list
+        return employeeDTOs;
     }
 
     // Method to convert a customer entity into Data Transfer Object
@@ -86,6 +107,20 @@ public class UserController {
         Customer customer = new Customer();
         BeanUtils.copyProperties(customerDTO, customer);
         return customer;
+    }
+
+    // Method to convert an employee entity into Data Transfer Object
+    private static EmployeeDTO convertEmployeeEntityToDTO(Employee employee) {
+        EmployeeDTO employeeDTO = new EmployeeDTO();
+        BeanUtils.copyProperties(employee, employeeDTO);
+        return employeeDTO;
+    }
+
+    // Method to convert an employee DTO to an entity
+    private static Employee convertEmployeeDTOToEntity(EmployeeDTO employeeDTO) {
+        Employee employee = new Employee();
+        BeanUtils.copyProperties(employeeDTO, employee);
+        return employee;
     }
 
 }
