8fb6065ebe4e730b2ea5898bab0c9d397edad915
Ciaran McNally <ciaran.mcnally94@gmail.com>
2022-01-28 18:16:56 +0000

    Implemented methods to find schedules by pet and employee

--- src/main/java/com/cmcnally/critter/repository/ScheduleRepository.java
+++ src/main/java/com/cmcnally/critter/repository/ScheduleRepository.java
@@ -1,13 +1,24 @@
 package com.cmcnally.critter.repository;
 
+import com.cmcnally.critter.entity.Employee;
+import com.cmcnally.critter.entity.Pet;
 import com.cmcnally.critter.entity.Schedule;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  * Repository interface for Schedule
  */
 
 @Repository
-public interface ScheduleRepository extends CrudRepository<Schedule, Long> {
+public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
+
+    // Find a list of schedules by pet
+    List<Schedule> findAllByPetsContaining(Pet pet);
+
+    // Find a list of schedules by employee
+    List<Schedule> findAllByEmployeesContaining(Employee employee);
 }
